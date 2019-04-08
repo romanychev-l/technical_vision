1a95e07f2fa5c4c9bb9cb1750698ffad74456629
kira <kirakari1101@gmail.com>
2019-04-08 21:18:05 +0300

    To open the picture

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,11 @@
+import cv2 as cv
+import numpy as np
+
+# Load an color image in grayscale
+img = cv.imread('1.png',0)
+cv.imshow('image',img)
+cv.waitKey(0)
+cv.destroyAllWindows()
+print('done')
+
 print("Hello world")
